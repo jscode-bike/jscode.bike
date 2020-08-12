f6e515e37dd29838d40955d9b3eae7615fdb3675
M M Alam <alam@mmtutor.com>
2020-08-12 08:41:58 -0400

    fixes

--- src/components/CodeArena/LeftPanel/Results/Result.js
+++ src/components/CodeArena/LeftPanel/Results/Result.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import SyntaxHighlighter from "react-syntax-highlighter";
-import { vs2015 } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import Outputs from "./Outputs.js";
 
 const Result = ({ result: { description, unitTestResults } }) => {
   return (
@@ -15,7 +14,7 @@ const Result = ({ result: { description, unitTestResults } }) => {
               <ResultTextContainter>
                 {passed ? "☑ Passed" : `🅧 Not Passing: ${error.message}`}
               </ResultTextContainter>
-              {!!outputs.length && <Outputs outputs={outputs} />}
+              {!!outputs.length && <Outputs {...{ outputs }} />}
             </UnitTestContainer>
           );
         })}
@@ -26,69 +25,8 @@ const Result = ({ result: { description, unitTestResults } }) => {
 
 const ResultTextContainter = styled.div`
   font-family: monospace;
-  padding-bottom: var(--spacing-small);
   font-size: 1.4rem;
 `;
-
-const OutputsContainer = styled.div`
-  background-color: var(--bg-color-darker);
-  /* overflow-x: hidden; */
-  width: calc(
-    calc(100vw * 4 / 9) - calc(var(--spacing-medium) * 4) -
-      calc(var(--spacing-small) * 3)
-  );
-`;
-
-const Outputs = ({ outputs }) => {
-  return (
-    <OutputsContainer>
-      <ConsoleHeading>console:</ConsoleHeading>
-      {outputs.map((o, idx) => {
-        const codeString = `${o.args.map((a) => a.text).join(" ")}`;
-        return (
-          <ConsoleOutputContainer key={idx}>
-            <SyntaxHighlighterContainer
-              language="javascript"
-              style={vs2015}
-              customStyle={{
-                margin: 0,
-                padding: "var(--spacing-small)",
-                backgroundColor: "var(--bg-color-darker)",
-              }}
-            >
-              {codeString}
-            </SyntaxHighlighterContainer>
-          </ConsoleOutputContainer>
-        );
-      })}
-    </OutputsContainer>
-  );
-};
-
-const ConsoleHeading = styled.h4`
-  font-family: monospace;
-  padding: var(--spacing-small);
-  background-color: var(--bg-color-dark);
-`;
-
-const SyntaxHighlighterContainer = styled(SyntaxHighlighter)`
-  ::-webkit-scrollbar {
-    width: var(--spacing-small);
-    height: var(--spacing-small);
-    cursor: pointer;
-  }
-  ::-webkit-scrollbar-track {
-    background-color: var(--bg-color-darker);
-  }
-  ::-webkit-scrollbar-thumb {
-    background-color: var(--bg-color);
-  }
-  ::-webkit-scrollbar-thumb:hover {
-    background-color: var(--bg-color-dark);
-  }
-`;
-
-const ConsoleOutputContainer = styled.div``;
 
 const UnitTestContainer = styled.div`
   align-self: stretch;
