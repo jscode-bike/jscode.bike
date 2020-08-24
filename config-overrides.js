dd8c6575f380a5018b85d01f1adc8e101efa9684
M M Alam <alam@mmtutor.com>
2020-08-24 06:16:34 -0400

    add comments for typescript config in monaco build pipeline

--- config-overrides.js
+++ config-overrides.js
@@ -4,7 +4,13 @@ const rewireFrontmatterMarkdown = require("react-app-rewire-frontmatter-markdown
 module.exports = function override(config, env) {
   config.plugins.push(
     new MonacoWebpackPlugin({
-      languages: ["json", "javascript"],
+      languages: [
+        "json",
+        "javascript",
+        /// below is optional for richer autocompletion...
+        /// see if you can make this a configurable
+        // "typescript",
+      ],
     })
   );
   rewireFrontmatterMarkdown(config, {
