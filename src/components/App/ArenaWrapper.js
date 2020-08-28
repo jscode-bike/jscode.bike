c1cf4afe2298ee36290205e8352c8ea0e8bd9f80
M M Alam <alam@mmtutor.com>
2020-08-28 14:44:18 -0400

    refactor problems for lazy loading

--- src/components/App/ArenaWrapper.js
+++ src/components/App/ArenaWrapper.js
@@ -1,32 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
-import { obj as exerciseHash } from "../../exercises/index.js";
+import exercises from "../../exercises/index.js";
 
 import CodeArena from "../CodeArena/CodeArena.js";
 import ArenaProvider from "../CodeArena/ArenaContext";
 import NoMatch from "./NoMatch.js";
+import Spinner from "../shared/Spinner.js";
 
 const ArenaWrapper = () => {
   const { variableName } = useParams();
-  const exercise = exerciseHash[variableName];
-  if (!exercise) return <NoMatch />;
-  debugger;
+  const [exercise, setExercise] = useState(null);
 
-  // const { startingCode, tests, instructionComponent } = exercise;
-  // return (
-  //   <ArenaProvider
-  //     {...{
-  //       startingCode,
-  //       tests,
-  //       variableName,
-  //       instructionComponent,
-  //     }}
-  //   >
-  //     <CodeArena {...exercise} />
-  //   </ArenaProvider>
-  // );
-  return <div></div>;
+  useEffect(() => {
+    exercises
+      .get(variableName)
+      .then((e) => {
+        setExercise(e);
+      })
+      .catch((_) => {
+        setExercise(false);
+      });
+  }, [variableName]);
+
+  if (exercise === null) return <Spinner />;
+  if (!exercise) return <NoMatch />;
+
+  const { startingCode, tests, instructionComponent } = exercise;
+  return (
+    <ArenaProvider
+      {...{
+        startingCode,
+        tests,
+        variableName,
+        instructionComponent,
+      }}
+    >
+      <CodeArena {...exercise} />
+    </ArenaProvider>
+  );
 };
 
 export default ArenaWrapper;
