eeb7ac91d933ba06325d87c56c4c62595309af94
M M Alam <alam@mmtutor.com>
2020-08-29 05:12:12 -0400

    improve no match rought and speed up transition

--- src/components/App/NoMatch.js
+++ src/components/App/NoMatch.js
@@ -1,7 +1,39 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import Button from "../shared/Button.js";
 
 const NoMatch = () => {
-  return <div>no match found</div>;
+  return (
+    <Container>
+      <Message>Uh oh... no page found :(</Message>
+      <StyledLink to="/">
+        <HomeButton>Home</HomeButton>
+      </StyledLink>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 75vh;
+`;
+
+const Message = styled.h2`
+  margin: var(--spacing-medium);
+`;
+
+const HomeButton = styled(Button)`
+  background-color: var(--bg-color-dark);
+  padding: var(--spacing-small) var(--spacing-medium);
+`;
+
+const StyledLink = styled(Link)`
+  color: var(--text-color);
+  text-decoration: none;
+`;
 
 export default NoMatch;
--- src/components/shared/OpacityTransition.js
+++ src/components/shared/OpacityTransition.js
@@ -12,6 +12,7 @@ const OpacityTransition = ({ children }) => {
     from: { opacity: 0 },
     enter: { opacity: 1 },
     leave: { opacity: 0 },
+    config: { duration: 100 },
   });
   return t.map(({ item: c, key: k, props: p }) => {
     return (
