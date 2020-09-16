e5af3e7cc4b36beec27cee0470c9060749e667a5
M M Alam <alam@mmtutor.com>
2020-09-16 00:50:47 -0400

    cleanup and docs

---
+++ src/runner/readme.md
@@ -0,0 +1,24 @@
+# Runner
+
+## API
+
+the runner has simper api:
+
+- `submitCode(code, tests, variableName, submissionId) => Promise<results>`
+- `refreshWorker() => Promise<void>`
+
+## Organization
+
+there are three different versions of the runner:
+
+1. mobile version (legacy runner)
+2. blob version (patched and concatenated to avoid `importScripts`)
+3. standard version (uses imports, js files in public directory)
+
+we ship either the mobile version or blob version in prod based off OS detection. standard version will start shipping once I figure out caching. until then, i will add all new features to standard and periodically update blob manually.
+
+## Architecture
+
+### Mobile (legacy)
+
+this is the first version of the code runner I came up with; just 1 worker which is killed after every test
--- src/runner/runner.js
+++ src/runner/runner.js
@@ -1,23 +1,8 @@
-// import submitCode from "./standard/submitCodeStandard.js";
-// import { refreshWorker as rw } from "./standard/submitCodeStandard.js";
-
 import getMobileOperatingSystem from "../utils/getMobileOS";
-
-// import submitCode from "./mobile/submitCodeMobile.js";
-// import { refreshWorker as rw } from "./mobile/submitCodeMobile.js";
-
-// import submitCode from "./blob/submitCodeBlob.js";
-// import { refreshWorker as rw } from "./blob/submitCodeBlob.js";
-
-/// both methods in runner return promises
-// find a way to feature detect and code split based on OS
 
 const isMobileOS = getMobileOperatingSystem() !== "other";
 
-console.log("is this a mobile OS?", isMobileOS);
-
 const submitCode = (...args) => {
-  console.log(args);
   return new Promise((resolve, reject) => {
     if (isMobileOS) {
       import("./mobile/submitCodeMobile.js")
@@ -31,7 +16,6 @@ const submitCode = (...args) => {
       import("./blob/submitCodeBlob.js")
         .then(async (submitCodeBlobData) => {
           const val = await submitCodeBlobData.default(...args);
-          console.log(val);
           resolve(val);
         })
         .catch((e) => {
