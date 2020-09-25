f07d196b75d533e53c3272b125ac1f3be3a0f394
M M Alam <alam@mmtutor.com>
2020-09-25 18:19:54 -0400

    clean up

--- drafts/solutions/rottenOranges/solution1.js
+++ drafts/solutions/rottenOranges/solution1.js
@@ -5,26 +5,15 @@ const orangesRotting = (g) => {
   let i = 0;
   while (cycleResult.hasChanged) {
     i++;
-    cycleResult = runSingleCycle(cycleResult.newGrid, cycleResult.newTwos);
+    cycleResult = runSingleCycle(cycleResult.newGrid);
   }
   return cycleResult.hasOnes ? -1 : i;
 };
 
-function gridHasOnes(g) {
-  for (let i = 0; i < g.length; i++) {
-    for (let j = 0; j < g[i].length; j++) {
-      if (g[i][j] === 1) return true;
-    }
-  }
-  return false;
-}
-
-const traverseWholeGrid = (grid) => {
+function runSingleCycle(grid) {
   const newGrid = grid.map((row) => [...row]);
   let hasChanged = false;
   let hasOnes = false;
-  const newTwos = [];
-
   for (let i = 0; i < grid.length; i++) {
     for (let j = 0; j < grid[i].length; j++) {
       const currCell = grid[i][j];
@@ -42,7 +31,6 @@ const traverseWholeGrid = (grid) => {
           const curr = grid[y][x];
           if (curr === 1) {
             hasChanged = true;
-            newTwos.push(coords);
             newGrid[y][x] = 2;
           }
         }
@@ -53,45 +41,7 @@ const traverseWholeGrid = (grid) => {
     hasChanged,
     newGrid,
     hasOnes,
-    newTwos,
   };
-};
-
-const justChangeNewTwos = (g, n2s) => {
-  const newGrid = g.map((row) => [...row]);
-  let hasChanged = false;
-  const newTwos = [];
-
-  for (let i = 0; i < n2s.length; i++) {
-    const currCellCoords = n2s[i];
-    const [x, y] = currCellCoords;
-    const adjacentCellsCoords = getAdjacentCells(x, y, g[y].length, g.length);
-
-    for (const coords of adjacentCellsCoords) {
-      const [x, y] = coords;
-      const curr = g[y][x];
-      if (curr === 1) {
-        hasChanged = true;
-        newTwos.push(coords);
-        newGrid[y][x] = 2;
-      }
-    }
-  }
-
-  return {
-    hasChanged,
-    newGrid,
-    hasOnes: gridHasOnes(newGrid),
-    newTwos,
-  };
-};
-
-function runSingleCycle(grid, newTwos = []) {
-  if (newTwos.length > 0) {
-    return justChangeNewTwos(grid, newTwos);
-  } else {
-    return traverseWholeGrid(grid);
-  }
 }
 
 function getAdjacentCells(x, y, w, h) {
